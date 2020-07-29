41c405c8152037c3af9bf4f5e142d8ce508e4f5c
Gaines Yeilding <gfy613@gmail.com>
2020-07-28 20:49:50 -0400

    Heroku update v4

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgerController.js");
 
 var PORT = process.env.PORT || 3000;
 
@@ -10,7 +12,7 @@ var app = express();
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Set Handlebars.
@@ -19,8 +21,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgerController.js");
+
 
 app.use(routes);
 
